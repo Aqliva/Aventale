0e90a0d9e8716180b7a0a20912a02d9f6547796b
Azerall <floria.lim75013@gmail.com>
2025-04-24 14:46:57 +0200

    managed 404 error

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const redirect = sessionStorage.getItem("redirectAfter404");
+if (redirect) {
+  sessionStorage.removeItem("redirectAfter404");
+  window.history.replaceState(null, '', redirect);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
