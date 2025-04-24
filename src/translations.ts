cb617608b47a6743dc19a886902851c37f608609
Azerall <floria.lim75013@gmail.com>
2025-04-24 15:41:15 +0200

    changed header with language

---
+++ src/translations.ts
@@ -0,0 +1,21 @@
+export const TRANSLATIONS = {
+  fr: {
+    previous: "Précédent",
+    next: "Suivant",
+    home: "Accueil",
+    selectChapter: "Sélectionner un chapitre",
+  },
+  en: {
+    previous: "Previous",
+    next: "Next",
+    home: "Home",
+    selectChapter: "Select a chapter",
+  },
+  zh: {
+    previous: "上一章",
+    next: "下一章",
+    home: "首页",
+    selectChapter: "选择章节",
+  },
+};
+  
